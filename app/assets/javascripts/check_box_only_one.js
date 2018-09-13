3c910a2a4735197ab681ba6f01096c62f6773b67
mao n <mao.nishikawa24@gmail.com>
2018-09-13 16:25:05 +0900

    checkbox_js_add

---
+++ app/assets/javascripts/check_box_only_one.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+$(".checkbox").on("click", function(){
+$('.checkbox').prop('checked', false);  //  全部のチェックを外す
+$(this).prop('checked', true);  //  押したやつだけチェックつける
+});
+
+</script>
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( get_location.js )
+Rails.application.config.assets.precompile += %w( check_box_only_one.js )
