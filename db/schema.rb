0fed00d92167dc4aa23468c35a2343d703562b20
mao n <mao.nishikawa24@gmail.com>
2018-09-20 13:33:42 +0900

    user_registration_1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911103339) do
+ActiveRecord::Schema.define(version: 20180920042905) do
 
   create_table "current_users", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20180911103339) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "tel",                    limit: 255
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
