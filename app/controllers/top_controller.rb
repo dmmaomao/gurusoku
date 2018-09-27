11c28bff3f2b7de9cf954224f0a10bde2caec44c
mao n <mao.nishikawa24@gmail.com>
2018-09-27 10:50:18 +0900

    user_reviews_1_views

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,34 @@
 class TopController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+  def reviews_params
+  end
+
+  def move_to_index
+    redirection_to action: :
+    index unless
+    user_signed_in?
+    end
+  end
+
 end
